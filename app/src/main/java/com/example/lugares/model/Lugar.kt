bb9bc129692dd727a5dd8e6ab4cb303fd0873cb6
LungChang <lung@xpressprocessing.com>
2022-10-19 01:36:42 -0600

    clase 4

---
+++ app/src/main/java/com/example/lugares/model/Lugar.kt
@@ -0,0 +1,31 @@
+package com.example.lugares.model
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+@Parcelize
+@Entity(tableName = "lugar")
+
+data class Lugar(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo(name="nombre")
+    val nombre: String,
+    @ColumnInfo(name="correo")
+    val correo: String?,
+    @ColumnInfo(name="telefono")
+    val telefono: String?,
+    @ColumnInfo(name="web")
+    val web: String,
+    @ColumnInfo(name="latitud")
+    val latitud: Double?,
+    @ColumnInfo(name="longitud")
+    val longitud: Double?,
+    @ColumnInfo(name="altura")
+    val altura: Double?,
+    @ColumnInfo(ma,e="rutaAudio")
+    val rutaAudio: String?,
+    @ColumnInffo(name="rutaImagen")
+    val rutaImagen: String?,
+) : Parcelable
